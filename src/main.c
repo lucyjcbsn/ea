495683f4af702f9fb8d1ab25e5040ea6049383bd
lucyjcbsn <berg.feltstykket@icloud.com>
2026-09-09 06:40:43 +0200

    oh my github

--- src/main.c
+++ src/main.c
@@ -1,1 +1,21 @@
 // fuck
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+const char byte_conv[16] = "123456789abcdef"
+
+char b;
+char h;
+
+
+
+int main(int argc, char *argv){
+	//args
+	
+	//open file as bin
+};
+
+int interface() {
+	
+};
