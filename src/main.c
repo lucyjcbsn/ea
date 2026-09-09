46bf314e1e98127bafdfc6f612c0035f74ea91e9
lucyjcbsn <berg.feltstykket@icloud.com>
2026-09-09 22:41:29 +0200

    yu me no yu me not you yu

--- src/main.c
+++ src/main.c
@@ -5,18 +5,14 @@
 #include <ncurses.h>
 
 const char byte_conv[16] = "123456789abcdef"
-const char args[] = {
-	"o",
-	"output",
-	"d",
-	"dump"
-}
 
-char b;
-char h;
+char convert(char b_c[1024]){
 
-char convert(char b_c){
-	unsigned long b_c_len;
+	char *res_h = NULL;
+	unsigned long res_offs;
+	unsigned long res_size;
+
+	unsigned long b_c_len = strlen(b_c);
 	
 	for(unsigned long i = 0; i < b_c_len; i += 8){
 		unsigned long byte_i = 0;
@@ -26,12 +22,16 @@ char convert(char b_c){
 			byte_i |= (1 << (7 - j));
 		}
 	}
+
+	if(res_offs + 4 >= res_size) {
+		res_size += 1024;
+		res_h = realloc(res_h, res_size);
+	}
+
+	return *res_h;
 };
 
 int main(int argc, char *argv){
-	//args
-	
-		
 	
 	//open file as bin
 	
@@ -41,10 +41,11 @@ int main(int argc, char *argv){
 	unsigned long read_i;
 	
 	while((bytes_read = fread(buffer, 1, sizeof(buffer), file))  > 0){
-		for() {
-		
+		for(unsigned long i = 0 < read_i: i++) {
+			convert(b_f_buffer);
 		}
 	}
+	return 1; 
 };
 
 int interface() {
