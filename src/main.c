59f55fca5e68e5db261d890cfdcb6407d248b81c
lucyjcbsn <berg.feltstykket@icloud.com>
2026-09-09 10:05:24 +0200

    klkjlkjlkj

--- src/main.c
+++ src/main.c
@@ -2,18 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ncurses.h>
 
 const char byte_conv[16] = "123456789abcdef"
+const char args[] = {
+	"o",
+	"output",
+	"d",
+	"dump"
+}
 
 char b;
 char h;
 
+char convert(char b_c){
+	unsigned long b_c_len;
+	
+	for(unsigned long i = 0; i < b_c_len; i += 8){
+		unsigned long byte_i = 0;
 
+		for(int j = 0; j < 8; j++){
+			if (b_c[i+j] == '1')
+			byte_i |= (1 << (7 - j));
+		}
+	}
+};
 
 int main(int argc, char *argv){
 	//args
 	
+		
+	
 	//open file as bin
+	
+	FILE b_f; = fopen(arg_f, "rb")
+		
+	char b_f_buffer[1024];
+	unsigned long read_i;
+	
+	while((bytes_read = fread(buffer, 1, sizeof(buffer), file))  > 0){
+		for() {
+		
+		}
+	}
 };
 
 int interface() {
